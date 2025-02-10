9b86cdba0b81ed7bfdf686a643de130d83502dc4
david4473 <omotayodavid9@gmail.com>
2025-02-10 01:52:04 +0100

    initial commit

---
+++ src/assets/index.js
@@ -0,0 +1,24 @@
+import bg from "./bg.jpg";
+
+import picture from "./picture.png";
+import picture_1 from "./picture_1.jpg";
+import picture_2 from "./picture_2.jpg";
+import picture_3 from "./picture_3.jpg";
+import picture_4 from "./picture_4.jpg";
+import picture_5 from "./picture_5.jpg";
+
+import confetti from "./confetti.gif";
+
+import CakeSVG from "./svg/CakeSVG";
+
+export {
+  bg,
+  picture,
+  picture_1,
+  picture_2,
+  picture_3,
+  picture_4,
+  picture_5,
+  confetti,
+  CakeSVG,
+};
